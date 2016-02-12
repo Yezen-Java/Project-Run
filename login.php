763db6c725823677eb84972e62cb963e6367c7ef
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-02-12 18:05:03 +0000

    login page hosting

---
+++ login.php
@@ -0,0 +1,222 @@
+<?php
+include("database/Connect.php");
+if (isset($_POST['signup'])){
+$fname=$_POST['firstname']; 
+$lastname=$_POST['lastname'];
+$email=$_POST['form-create-email'];
+$username=$_POST['form-create-username'];
+$password= sha1($_POST['form-create-password']);
+$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+echo " Please Enter Vaild Email";
+}else{
+$resultemail = mysql_query("SELECT * FROM users WHERE Email='$email'");
+$resultuser = mysql_query("SELECT * FROM users WHERE Username='$username'");
+$dataEmail = mysql_num_rows($resultemail);
+$dataUsername = mysql_num_rows($resultuser);
+if(($dataEmail)==0 &&($dataUsername)==0 ){
+$query = mysql_query("INSERT into users (Firstname, Lastname,Email,Username, Password) values ('$fname', '$lastname','$email','$username','$password')"); // Insert query
+if($query){
+echo "You have Successfully Registered";
+}else
+{
+echo "Error!!";
+}
+}else{
+echo "This email is already registered, Please try another email";
+}
+}
+mysql_close ($connection);
+}
+
+if (isset($_POST['btlogin'])) {
+include("database/UserLoginSession.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Hive Login</title>
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
+        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="assets/css/form-elements.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="shortcut icon" href="assets/ico/favicon.png">
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <!-- JavaScript-->
+        
+    </head>
+    <style type="text/css">
+      .navbar {
+         margin-bottom: 0px;
+      }
+      #createAccountContainer{
+         width: 500px;
+         padding-bottom: 100px;
+         padding-right: 200px;
+      }
+
+      #mainLogin{
+         width: 500px;
+         padding-bottom: 100px;
+         padding-right: 200px;
+      }
+
+      .carousel-caption h1{
+         font-size: 5.4em;
+         font-family: 'Pacifico', cursive;
+         padding-bottom: .4em;
+      }
+      .carousel-caption p{
+         font-size: 2em;
+      }
+
+      .slide1{
+         height: 500px;
+         background-position: center;
+      }
+      .slide2{
+         height: 500px;
+         background-position: center;
+      }
+      #navigationButtons{
+       
+      }
+      .carousel-control.left, .carousel-control.right{
+         background-image: none;
+      }
+
+
+      body{
+         background-color: #F2DF07;
+      }
+
+      #parent-name {
+  display: flex;
+    }
+     #finame-div {
+     	width: 135px;
+     	float: left;
+ 
+      }
+    #laname-div {
+    float: right;
+
+  flex: 1;
+}
+    </style>
+    <body>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script type="text/javascript" src="database/registration.js"></script>
+    <script type="text/javascript">
+
+
+    </script>
+        <div class="top-content">
+            <div class="inner-bg">
+                <div class="container">
+                        <div class="page-header">
+                              <h1><strong>Hive</strong> Login</h1>
+                        </div>   
+                </div>
+            </div>
+            <div class="bottom-content">
+               <div class="container">
+                  <div id="theCarousel" class="carousel slide" data-ride="carousel">
+                     <ol class="carousel-indicators">
+                        <li data-target="#theCarousel" data-slide-to="0" class="active"> </li >
+                        <li data-target="#theCarousel" data-slide-to="1"> </li>
+                     </ol >
+                     <div class="carousel-inner">
+                     <div class="item active" id="firstCarousel">
+                        <div class ="slide1"></div>
+                        <div class="carousel-caption">
+                           <div class="col-sm-6 col-sm-offset-3 form-box">
+                           <form method="post" action="login.php">
+                              <div class="container" id="mainLogin">
+                                 <h2><strong>Login</strong></h2>
+                                    <div class="form-bottom" id="loginForm">
+                                       <div class="form-group">
+                                          <input type="text" name="form-username" placeholder="Username" class="form-username form-control" >
+                                       </div>
+                                       <div class="form-group">
+                                          <input type="password" name="form-password" placeholder="Password" class="form-password form-control">
+                                       </div>
+                                        <div class="btn-group">
+                                          <button class="btn btn-primary btn-lg" name="btlogin">Sign in</button>
+                                          <button class="btn btn-primary btn-lg">Create Account</button>   
+                                        </div>
+                                    </div>
+                              </div>
+                              </form>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="item">
+                           <div class="slide2"></div>
+                            <div class="carousel-caption">
+                              <div class="col-sm-6 col-sm-offset-3 form-box">
+                              <form method="post">
+                                 <div class="container" id="createAccountContainer">
+                                    <h3><strong>Create</strong> account</h3>
+
+                                    <div class="form-group" id="parent-name">
+                                    <div id="finame-div"> <input type="text" class="form-control" name="firstname" placeholder="First Name" id="Cname">
+                                    </div>
+                                    <div id="laname-div"><input type="text" class="form-control" name="lastname" placeholder="Last Name" id="Clname"></div>
+                                       
+                                    </div>
+                                    <div class="form-group">
+                                       <input type="text" name="form-create-email" placeholder="Email" class="form-create-email form-control" id="form-email-css" >
+                                    </div>
+                                    <div class = "form-group">
+                                       <input type="text" name="form-create-username" placeholder="Username" class="form-create-username form-control" id="form-user-css">
+                                    </div>
+                                    <div>
+                                    <div class = "form-group">
+                                       <input type="password" name="form-create-password" placeholder="Password" class="form-create-password form-control" id="form-password-css">
+                                    </div>
+                                    <div class = "form-group">
+                                       <input type="password" name="form-create-password2" placeholder="Password" class="form-create-password form-control" id="form-password-css">
+                                    </div>
+                                    </div>
+                                       <button class="btn btn-primary btn-lg" name = "signup">Create</button>
+                                 </div>  
+                                 </form>
+                              </div>
+                           </div>
+                        </div>
+                        <a class="left carousel-control" href="#theCarousel" data-slide="prev">
+                           <span class="glyphicon glyphicon-chevron-left"> </span>
+                        </a>
+                        <a class="right carousel-control" href="#theCarousel" data-slide="next">
+                           <span class="glyphicon glyphicon-chevron-right"></span>
+                        </a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+        </div>
+        <script src="assets/js/jquery-1.11.1.min.js"></script>
+        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+        <script src="assets/js/jquery.backstretch.min.js"></script>
+        <script src="assets/js/scripts.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    </body>
+
+</html>
