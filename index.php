16303c2f1403bbf5a378c1747babe5fcafa6696b
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-02-12 18:21:06 +0000

    index page

---
+++ index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-7s5uDGW3AHqw6xtJmNNtr+OBRJUlgkNJEo78P4b0yRw= sha512-nNo+yCHEyn0smMxSswnf/OnX6/KwJuZTlNZBjauKhTK0c+zT+q5JOCx0UFhXQ6rJR9jg6Es8gPuD2uZcYDLqSw==" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha256-KXn5puMvxCw+dAYznun+drMdG1IFl3agK0p/pqT9KAo= sha512-2e8qq0ETcfWRI4HJBzQiA3UoyFk6tbNyG+qSaIBZLyW9Xf3sWZHN/lxe9fTh1U45DpPf07yj94KsUHHWe4Yk1A==" crossorigin="anonymous"></script> 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyA5UfBMd4ogo--f-j_ysGFxiQT2h2cJwnA"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="main.css">
+
+    <link href="simple-sidebar.css" rel="stylesheet">
+    <script
+        src="https://www.google.com/maps/d/embed?mid=zdaGz_kxHGcY.kNA-vyp_PChI">
+    </script>
+
+    <script>
+    var map;
+    var myCenter=new google.maps.LatLng(51.48963307250382, -0.1708325743675232);
+    var circles = [];
+    var uniqueId = 1;
+
+    function initialize(){
+    var mapProp = {
+      center:myCenter,
+      zoom:18,
+      mapTypeId:google.maps.MapTypeId.ROADMAP
+      };
+
+      map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+      google.maps.event.addListener(map, 'click', function(event) {
+        placeMarker(event.latLng);
+        var location = event.latLng;
+      });
+    }
+
+    function placeMarker(location) {
+
+        var circle = new google.maps.Circle({
+            center:location,
+            map:map,
+            position:location,
+            radius:10,
+            strokeColor:"#0000FF",
+            strokeOpacity:0.8,
+            strokeWeight:2,
+            fillColor:"#0000FF",
+            fillOpacity:0.4
+        });
+
+        circle.id = uniqueId;
+        uniqueId++;
+  
+
+    google.maps.event.addListener(circle, "click", function (e){
+        var content = 'Latitude: ' + location.lat() + '<br>Longitude: ' + location.lng();
+        content += "<br /><input type = 'button' va;ua = 'Delete' onclick = 'DeleteCircle(" + circle.id + ");' value = 'Delete' />";
+        var infowindow = new google.maps.InfoWindow({
+            content: content
+        });
+        infowindow.open(map,circle);
+      });
+
+    circles.push(circle);
+      
+    } 
+
+    function DeleteCircle(id){
+        for (var i = 0; i < circles.length; i++){
+            if (circles[i].id == id){
+                circles[i].setMap(null);
+                circles.splice(i, 1);
+                return;
+            }
+        }
+    };
+    
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+    
+</script>
+</head>
+
+<body>
+<div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+          <div class="masthead clearfix">
+            <div class="inner">
+                <nav class="navbar navbar-default navbar-fixed-top">
+                  <div class="container">
+                    <div class="navbar-header">
+                      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                      </button>
+                      <a class="navbar-brand">Project name</a>
+                    </div>
+                    <div id="navbar" class="navbar-collapse collapse">
+                      <ul class="nav navbar-nav">
+                        <li class="active"><a>Home</a></li>
+                        <li><a value="Show Dialog" onclick="$('#myInput').click();">Upload</a></li>
+                        <li><a href="#" id="EditTourButton">Edit Tour</a></li>
+                      </ul>
+                    </div><!--/.nav-collapse -->
+                  </div>
+                </nav>
+            </div>
+          </div>
+            <div id="googleMap"></div>
+        </div>
+        <input id="myInput" type="file" style="visibility:hidden"/>
+
+    </div>
+
+</div>
+
+</body>
+    <script type="text/javascript" src="script.js"></script>
+
+</html>
+
