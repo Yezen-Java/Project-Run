f731922814727f5c849379697907eaca5f10010f
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-02-13 20:48:51 +0000

    adding database postgres

--- login.php
+++ login.php
@@ -1,7 +1,16 @@
 <?php
 
+$dbconn = pg_connect("host=ec2-107-21-221-59.compute-1.amazonaws.com dbname=da2vmjb6giivfh user=enybctwamdyitl
+ password=z3paibkPjPYeWNWib9d3nD0Pi8")
+or die('Could not connect: ' . pg_last_error());
+
+if ($dbconn ) {
+
+echo "Databse Connected";
+}
+
 if (isset($_POST['signup'])){
-include("database/Connect.php");
+
 $fname=$_POST['firstname']; 
 $lastname=$_POST['lastname'];
 $email=$_POST['form-create-email'];
