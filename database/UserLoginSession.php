19b9bf75f3fd433b18122e701417380d2efeb5a0
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-02-13 21:41:12 +0000

    fixing bugs

--- database/UserLoginSession.php
+++ database/UserLoginSession.php
@@ -5,14 +5,13 @@ $username = $_POST['form-username'];
 $password = $_POST['form-password'];
 $result = pg_query("SELECT * FROM users WHERE Username='$username'");
 
-
 if ($result) {
 	$row = pg_fetch_row($result);
 	$userId = $row[0];
-	$usernameR = $row[4];
+	$usernameR = $row[3];
 	$passwordR = $row[5];
 }
-if($username == $usernameR  && sha1($password) == $passwordR){
+if($username === $usernameR  && sha1($password) === $passwordR){
 	$_SESSION['username'] = $username;
 	$_SESSION['id'] = $userId;
    header('Location: index.php');
