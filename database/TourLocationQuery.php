0d55bd9fc1f27a17a541bc78d00eab585bc0ff7b
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-02-18 21:48:42 +0000

    added location query

--- database/TourLocationQuery.php
+++ database/TourLocationQuery.php
@@ -1,7 +1,7 @@
 <?php 
 
 $tourCode = pg_escape_string($_POST['TourCode']);
-$result = pg_query( "SELECT lname, latitude, logitude from tour_res tr, location l where tr.tourid = '1' and tr.locationid = l.locationid");
+$result = pg_query( "SELECT * from tour r, tour_res tr, location l where r.tourgode = '$tourCode' and tr.tourid = r.tourid and tr.locationid = l.locationid;");
 echo $result;
 
 ?>
