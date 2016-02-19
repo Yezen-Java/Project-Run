f81240b071414b8205d7f57c840c0802b0275dc8
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-02-19 15:42:17 +0000

    Update repositry

--- index.php
+++ index.php
@@ -90,14 +90,12 @@ echo "Tour query passed. ";
         var location = event.latLng;
       });
     }
- 
- 	
 
-    function placeMarker(cursorLocation) {
+    function placeMarker(location) {
         var circle = new google.maps.Circle({
-            center:cursorLocation,
+            center:location,
             map:map,
-            position:cursorLocation,
+            position:location,
             radius:10,
             strokeColor:"#0000FF",
             strokeOpacity:0.8,
@@ -112,6 +110,23 @@ echo "Tour query passed. ";
       	});
       
     } 
+
+    function circleMarker(currentLocation) {
+        var circle = new google.maps.Circle({
+            center:currentLocation,
+            map:map,
+            position:currentLocation,
+            radius:10,
+            strokeColor:"#0000FF",
+            strokeOpacity:0.8,
+            strokeWeight:2,
+            fillColor:"#0000FF",
+            fillOpacity:0.4
+        });
+        	whichCircle = circle.id;
+        	$("#myModal").modal();  	
+    }
+
     function DeleteCircle(){
         for (var i = 0; i < circles.length; i++){
             if (circles[i].id == whichCircle){
