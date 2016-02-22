9c9200c8a8628f57ab0c7ccb3277212bcd09596e
Sedar Olmez <SedarOlmez@Sedars-MacBook-Air.local>
2016-02-22 19:19:06 +0000

    connected to repo

--- index.php
+++ index.php
@@ -146,14 +146,16 @@ echo "Tour query passed. ";
   <nav class="navbar navbar-default navbar-fixed-top">
     <div class="container">
       <div class="navbar-header" id="topNavbar">
-      	<div class=".col-md-12" id="topNavbarContainer">
+      	<div class=".col-md-9" id="topNavbarContainer">
         	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
           		<span class="sr-only">Toggle navigation</span>
           		<span class="icon-bar"></span>
           		<span class="icon-bar"></span>
           		<span class="icon-bar"></span>
         	</button>
-        	<a class="navbar-brand"><strong>Hive</strong> Dashboard</a> 
+      	</div>
+      	<div class=".col-md-3">
+      		<a class="navbar-brand"><strong>Hive</strong> Dashboard</a> 
       	</div>
       </div>
       <div id="navbar" class="navbar-collapse collapse">
