feeb2f2316cc94104b5ac2ada11c7542f99bffd9
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-02-23 22:19:38 +0000

    added connect php file

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
-$dbconn = pg_connect("host=ec2-107-21-221-59.compute-1.amazonaws.com dbname=da2vmjb6giivfh user=enybctwamdyitl
- password=z3paibkPjPYeWNWib9d3nD0Pi8")
-or die('Could not connect: ' . pg_last_error());
+//$dbconn = pg_connect("host=ec2-107-21-221-59.compute-1.amazonaws.com dbname=da2vmjb6giivfh user=enybctwamdyitl
+ //password=z3paibkPjPYeWNWib9d3nD0Pi8")
+//or die('Could not connect: ' . pg_last_error());
+
+include 'database/Connect.php';
 
 error_reporting(E_ALL & ~E_NOTICE);
 session_start();
