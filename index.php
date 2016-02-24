85aec89a7bf5620871ae8b253d389b79dd279e1f
Sedar Olmez <SedarOlmez@Sedars-MacBook-Air.local>
2016-02-24 14:03:53 +0000

    GFSGFS

--- index.php
+++ index.php
@@ -119,10 +119,11 @@ echo "Tour query passed. ";
             ?>
         </ul>
       </div>
-      </div>
+      
       <div class="btn-group" role="group" aria-label="Basic example" id="tourButtonGroup">
           <button id="deleteTour" type="button" class="btn btn-danger"><strong>Delete</strong></button>
           <button id="addTour" type="button" class="btn btn-success" data-toggle="modal" data-target="#createTourDialogue"><strong>Add</strong></button>
+      </div>
       </div>
       </div>
     <!--Below we have the notes nav bar wrapped, here we should be able to add new notes, delete notes and
