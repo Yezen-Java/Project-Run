142952b7851f9bc1662e676ab2ff24e084f36e96
Sedar Olmez <SedarOlmez@Sedars-MacBook-Air.local>
2016-02-24 23:22:09 +0000

    fixing layout

--- index.php
+++ index.php
@@ -100,8 +100,8 @@ echo "Tour query passed. ";
       <!--Below we have a list-group which includes pre-made tours, we must add functionality to the Add 
       tour button so that when we click the button, a function should add a new button to the end of the
       list group-->
-      <div class=".col-xs-9 .col-sm-6 .col-md-9" id="outerContainer">
-      <div class = "container" id ="containerMiddleButtons">
+      <div class=".col-xs-9 .col-sm-6 .col-md-9">
+      <div class = "container">
       <div class="list-group" id="tourList">
         <ul id="buttonsListTours">
           <li>
