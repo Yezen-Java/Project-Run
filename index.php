b3a82974598fd597276140619999130561a7cbbb
Sedar Olmez <SedarOlmez@Sedars-MacBook-Air.local>
2016-02-24 14:41:45 +0000

    GFSGFS

--- index.php
+++ index.php
@@ -100,7 +100,7 @@ echo "Tour query passed. ";
       <!--Below we have a list-group which includes pre-made tours, we must add functionality to the Add 
       tour button so that when we click the button, a function should add a new button to the end of the
       list group-->
-      <div class = "container" id="middleContainer">
+      <div class = "container">
       <div class=".col-xs-9 .col-sm-6 .col-md-9">
       <div class="list-group" id="tourList">
         <ul id="buttonsListTours">
@@ -119,9 +119,12 @@ echo "Tour query passed. ";
             ?>
         </ul>
       </div>
+      </div>
+      <div class = "container">
       <div class="btn-group" role="group" aria-label="Basic example" id="tourButtonGroup">
           <button id="deleteTour" type="button" class="btn btn-danger"><strong>Delete</strong></button>
           <button id="addTour" type="button" class="btn btn-success" data-toggle="modal" data-target="#createTourDialogue"><strong>Add</strong></button>
+      </div>
       </div>
       </div>
       </div>
