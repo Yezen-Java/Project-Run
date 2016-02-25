8a30c969c70a5efce72f17efa7ceb4075c908481
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-02-25 22:14:41 +0000

    added locations for tours from sql

--- script.js
+++ script.js
@@ -83,17 +83,13 @@ if(window.File && window.FileReader && window.FileList && window.Blob){
     }
 
     function w3_open(value) {
-      alert(value);
         document.getElementsByClassName("w3-sidenav")[0].style.display = "block";
         $.post("database/GetTourLocations.php", {
-TourID: value
-}, function(data) {
-$("#pointersDiv").html(data);
+         TourID: value }, function(data) {
+          $("#pointersDiv").html(data);
 
 });
-      
-
-    }
+      }
     function w3_close() {
         document.getElementsByClassName("w3-sidenav")[0].style.display = "none";
     }
