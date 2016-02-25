dc5ca711a0bc38418e3ad28a468f576b999f639e
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-02-25 22:22:35 +0000

    added locations for tours from sql

--- database/GetTourLocations.php
+++ database/GetTourLocations.php
@@ -3,6 +3,8 @@
 include 'Connect.php';
 $TouridCode = $_POST['TourID'];
 $query = pg_query("SELECT * from tour_res tr, location l where tourid ='$TouridCode' and tr.locationid = l.locationid;");
+
+
 while($row = mysql_fetch_array($query)) {
 	$locationname = $row['lname'];
 
