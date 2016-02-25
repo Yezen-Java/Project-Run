1dbe97f1658fb98ffc6fea92f73512779c1454e1
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-02-25 21:11:28 +0000

    added locations for tours from sql

--- index.php
+++ index.php
@@ -107,7 +107,7 @@ echo "Tour query passed. ";
               $tour_id =$rows["tourid"];
               $tour_name =$rows["tour_name"];
 
-             echo "<li> <button type='button' name='tourButton' class='list-group-item' value='$tour_id' onclick='w3_open()'>$tour_name $tour_id</button> </li>";
+             echo "<li> <button type='button' id='tourButton' class='list-group-item' value='$tour_id' onclick='w3_open()'>$tour_name $tour_id</button> </li>";
               
               }  
               ?>
