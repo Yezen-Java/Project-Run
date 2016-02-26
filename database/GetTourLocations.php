4766bdc0318325c395627bb538887bcaa821c798
Hassan Minhas <haminhas@live.com>
2016-02-26 14:57:12 +0000

    checking jq php

--- database/GetTourLocations.php
+++ database/GetTourLocations.php
@@ -4,11 +4,16 @@ include 'Connect.php';
 $TouridCode = $_POST['TourID'];
 $query = pg_query("SELECT * from tour_res tr, location l where tourid ='$TouridCode' and tr.locationid = l.locationid;");
 
-
 while($row = mysql_fetch_array($query)) {
 	$locationname = $row['lname'];
+	$locationId = $row['locationid'];
 
-echo "<a>$locationname</a>";
+echo "<lu><li>id ='$locationId'>$locationname</li></lu>";
+}
+echo "good";
+
+if(!$query){
+	echo "No locations connect";
 }
 
 ?>
