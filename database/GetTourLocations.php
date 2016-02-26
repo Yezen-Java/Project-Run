cb3c6eaee06ad1919f90e3838748451e758da466
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-02-26 22:03:54 +0000

    added jqery for fetching data without refreshing the page

--- database/GetTourLocations.php
+++ database/GetTourLocations.php
@@ -3,7 +3,9 @@
 include 'Connect.php';
 $TouridCode = $_POST['value'];
 
-$query = pg_query("SELECT * from tour_res, location"."where tourid ='$TouridCode' and tour_res.locationid = location.locationid");
+$queryString = "SELECT * from tour_res, location where tourid ='$TouridCode' and tour_res.locationid = location.locationid";
+
+$query = pg_query($queryString);
 //$query = pg_query("SELECT * from location;");
 
 //if(pg_num_rows($query)==0){
