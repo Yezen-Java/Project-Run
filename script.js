c8aa935e9e5edbda4bbfe405d1f20b11ab5b1574
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-02-26 21:46:34 +0000

    added jqery for fetching data without refreshing the page

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ if(window.File && window.FileReader && window.FileList && window.Blob){
 
           $(function() {
           var value1 = $('#tourButton').val();
-           $.post('database/GetTourLocations.php',{value:value1}, function(data){
+           $.post('database/GetTourLocations.php',{value:TourIdCode}, function(data){
              $("#pointersDiv").html(data);
            });
            return false;
