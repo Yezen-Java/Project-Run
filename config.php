ec4e9d76e272e248c1456e918ef338d1e2bd8632
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-02-29 19:46:16 +0000

    added amazon s3 SDK and setup amazon s3 client access

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+return[
+
+'s3' => [
+     'key' => 'AKIAJZBB47YMO4T33EAA',
+     'secret' => '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',
+     'bucket' =>'storage.s3.website.com'
+
+]
+
+];
+
+?>
