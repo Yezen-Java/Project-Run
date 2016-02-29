74a0533b863ea8f05ee4aeae132974012906d597
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-02-29 21:58:55 +0000

    file upload to amazon s3 bug fix

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 return[
 
 's3' => [
-     'key' => 'AKIAJZBB47YMO4T33EAA',
-     'secret' => '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',
+     'key' => 'AKIAI7EBQYIK4VC7L7VA',
+     'secret' => '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',
      'bucket' => 'storage.s3.website.com'
 
 ]
