f68a4417e0875fc296a59e6feed21086652ed8dd
Sedar Olmez <SedarOlmez@Sedars-MacBook-Air.local>
2016-02-29 23:55:19 +0000

    Amazon AWS upload test Sedar

--- config.php
+++ config.php
@@ -4,8 +4,8 @@ $bucket="storage.s3.website.com";
 if (!class_exists('S3'))require_once('S3.php');
 
 //AWS access info
-if (!defined('awsAccessKey')) define('awsAccessKey', 'AKIAI7EBQYIK4VC7L7VA');
-if (!defined('awsSecretKey')) define('awsSecretKey', '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');
+if (!defined('awsAccessKey')) define('awsAccessKey', 'AKIAJQAUHJ7XGYHTS6AQ');
+if (!defined('awsSecretKey')) define('awsSecretKey', '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');
 
 $s3 = new S3(awsAccessKey, awsSecretKey);
 $s3->putBucket($bucket, S3::ACL_PUBLIC_READ);
