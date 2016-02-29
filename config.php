db2e7e03b08232f5c635b29cd49e49382dc870b6
Sedar Olmez <SedarOlmez@Sedars-MacBook-Air.local>
2016-02-29 23:42:38 +0000

    testing amazon s3 Sedar

--- config.php
+++ config.php
@@ -1,11 +1,12 @@
 <?php
+// Bucket Name
+$bucket="storage.s3.website.com";
+if (!class_exists('S3'))require_once('S3.php');
 
-return[
+//AWS access info
+if (!defined('awsAccessKey')) define('awsAccessKey', 'AKIAI7EBQYIK4VC7L7VA');
+if (!defined('awsSecretKey')) define('awsSecretKey', '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');
 
-	's3' => [
-     	'key' => 'AKIAI7EBQYIK4VC7L7VA',
-     	'secret' => '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',
-     	'bucket' => 'storage.s3.website.com'
-	]
-];
+$s3 = new S3(awsAccessKey, awsSecretKey);
+$s3->putBucket($bucket, S3::ACL_PUBLIC_READ);
 ?>
