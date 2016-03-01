8c196f80ccb79524f225c7fe09f49d4e060b9e80
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-03-01 13:50:16 +0000

    added amazon s3

--- config.php
+++ config.php
@@ -1,12 +1,30 @@
-<?php
+ <?php
 // Bucket Name
-$bucket="storage.s3.website.com";
-if (!class_exists('S3'))require_once('S3.php');
+//$bucket="storage.s3.website.com";
+//if (!class_exists('S3'))require_once('S3.php');
 
 //AWS access info
-if (!defined('awsAccessKey')) define('awsAccessKey', 'AKIAJQAUHJ7XGYHTS6AQ');
-if (!defined('awsSecretKey')) define('awsSecretKey', '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');
+//if (!defined('awsAccessKey')) define('awsAccessKey', 'AKIAJQAUHJ7XGYHTS6AQ');
+//if (!defined('awsSecretKey')) define('awsSecretKey', '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');
 
-$s3 = new S3(awsAccessKey, awsSecretKey);
-$s3->putBucket($bucket, S3::ACL_PUBLIC_READ);
+//$s3 = new S3(awsAccessKey, awsSecretKey);
+//$s3->putBucket($bucket, S3::ACL_PUBLIC_READ);
+ 
+return[
+
+'s3'=>[
+
+'key' => 'AKIAIPWMS2UDOEDVFTDQ',
+'Secret' => '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',
+'bucket' => 'storage.s3.website.com',
+
+
+]
+
+
+
+]
+
+
+
 ?>
