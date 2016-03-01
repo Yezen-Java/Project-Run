0406c858f58f660894ffae4d30b2645fbb25939e
Sedar Olmez <SedarOlmez@Sedars-MacBook-Air.local>
2016-03-01 16:01:40 +0000

    AWS Edit

--- config.php
+++ config.php
@@ -1,30 +1,13 @@
- <?php
-// Bucket Name
-//$bucket="storage.s3.website.com";
-//if (!class_exists('S3'))require_once('S3.php');
+<?php
 
-//AWS access info
-//if (!defined('awsAccessKey')) define('awsAccessKey', 'AKIAJQAUHJ7XGYHTS6AQ');
-//if (!defined('awsSecretKey')) define('awsSecretKey', '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');
+$bucket="storage.s3.website.com";
+if (!class_exists('S3'))require_once('S3.php');
+			
+if (!defined('awsAccessKey')) define('awsAccessKey', 'AKIAJQAUHJ7XGYHTS6AQ');
+if (!defined('awsSecretKey')) define('awsSecretKey', '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');
+			
+$s3 = new S3(awsAccessKey, awsSecretKey);
 
-//$s3 = new S3(awsAccessKey, awsSecretKey);
-//$s3->putBucket($bucket, S3::ACL_PUBLIC_READ);
- 
-return[
-
-'s3'=>[
-
-'key' => 'AKIAIPWMS2UDOEDVFTDQ',
-'Secret' => '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',
-'bucket' => 'storage.s3.website.com',
-
-
-]
-
-
-
-];
-
-
+$s3->putBucket($bucket, S3::ACL_PUBLIC_READ);
 
 ?>
