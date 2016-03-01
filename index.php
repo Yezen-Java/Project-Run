682766ce738c9bb826a383121edeec1eec4d3894
Sedar Olmez <SedarOlmez@Sedars-MacBook-Air.local>
2016-03-01 16:38:31 +0000

    AWS Test

--- index.php
+++ index.php
@@ -47,52 +47,52 @@ echo "Tour query passed. ";
   echo "Failed to Tour data. ";
 }
 //---------------------
-include('imageChecker/image_check.php');
-$msg='';
-if($_SERVER['REQUEST_METHOD'] == "POST")
-{
+// include('imageChecker/image_check.php');
+// $msg='';
+// if($_SERVER['REQUEST_METHOD'] == "POST")
+// {
 
-$name = $_FILES['file']['name'];
-$size = $_FILES['file']['size'];
-$tmp = $_FILES['file']['tmp_name'];
-$ext = getExtension($name);
+// $name = $_FILES['file']['name'];
+// $size = $_FILES['file']['size'];
+// $tmp = $_FILES['file']['tmp_name'];
+// $ext = getExtension($name);
 
-if(strlen($name) > 0)
-{
+// if(strlen($name) > 0)
+// {
 
-if(in_array($ext,$valid_formats))
-{
+// if(in_array($ext,$valid_formats))
+// {
  
-if($size<(1024*1024))
-{
-include('config.php');
-//Rename image name. 
-$actual_image_name = time().".".$ext;
-if($s3->putObjectFile($tmp, $bucket , $actual_image_name, S3::ACL_PUBLIC_READ) )
-{
-$msg = "S3 Upload Successful.";	
-$s3file='http://'.$bucket.'.s3.amazonaws.com/'.$actual_image_name;
-echo "<img src='$s3file' style='max-width:400px'/><br/>";
-echo '<b>S3 File URL:</b>'.$s3file;
+// if($size<(1024*1024))
+// {
+// include('config.php');
+// //Rename image name. 
+// $actual_image_name = time().".".$ext;
+// if($s3->putObjectFile($tmp, $bucket , $actual_image_name, S3::ACL_PUBLIC_READ) )
+// {
+// $msg = "S3 Upload Successful.";	
+// $s3file='http://'.$bucket.'.s3.amazonaws.com/'.$actual_image_name;
+// echo "<img src='$s3file' style='max-width:400px'/><br/>";
+// echo '<b>S3 File URL:</b>'.$s3file;
 
-}
-else
-$msg = "S3 Upload Fail.";
+// }
+// else
+// $msg = "S3 Upload Fail.";
 
 
-}
-else
-$msg = "Image size Max 1 MB";
+// }
+// else
+// $msg = "Image size Max 1 MB";
 
-}
-else
-$msg = "Invalid file, please upload image file.";
+// }
+// else
+// $msg = "Invalid file, please upload image file.";
 
-}
-else
-$msg = "Please select image file.";
+// }
+// else
+// $msg = "Please select image file.";
 
-}
+// }
 
 ?>
 
