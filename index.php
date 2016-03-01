00752c9c8e01c0ca808c70e74292c2770aa8e2cc
Sedar Olmez <SedarOlmez@Sedars-MacBook-Air.local>
2016-03-01 16:32:02 +0000

    AWS Test

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
 //$dbconn = pg_connect("host=ec2-107-21-221-59.compute-1.amazonaws.com dbname=da2vmjb6giivfh user=enybctwamdyitl
  //password=z3paibkPjPYeWNWib9d3nD0Pi8")
  //or die('Could not connect: ' . pg_last_error());
-include 'imageChecker/image_check.php';
+//include 'imageChecker/image_check.php';
 include 'database/Connect.php';
-include 'config.php';
+//include 'config.php';
 
 //require 'start.php';
 
