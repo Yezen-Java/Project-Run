be182e116ba79970c14e2bfdc8e1b43b96e59f15
Hassan Minhas <haminhas@live.com>
2016-03-03 12:02:38 +0000

    bug5

--- index.php
+++ index.php
@@ -141,7 +141,6 @@ echo "Tour query passed. ";
           <a href="javascript:void(0)" onclick="w3_close()" class="w3-closenav w3-large">Close &times;</a>
          <div id="pointersDiv">Locations</div>
           <div class="btn-group" role="group" aria-label="Basic example" id="buttonGroupPointer">
-            <button id="deletePointer" type="button" class="btn btn-danger"><strong>Delete</strong></button>
             <button id="addPointer" type="button" class="btn btn-success"><strong>Add</strong></button>
           </div>    
         </nav>
