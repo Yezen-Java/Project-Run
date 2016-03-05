8ef3e530a39e6389b4afa48b0c176f4691e19888
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-03-05 14:27:39 +0000

    js

--- index.php
+++ index.php
@@ -111,6 +111,7 @@ echo "Tour query passed. ";
 
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
 
     <!--CSS LINK-->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
