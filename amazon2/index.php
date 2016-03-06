3486e985b521830474f8f16e040949a9ce270bb6
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-03-06 09:19:10 +0000

    sorted out, the mulitaple file uplaod, i have linked it in test folder amazonz so it can be added to out code

--- amazon2/index.php
+++ amazon2/index.php
@@ -3,46 +3,57 @@ include('image_check.php');
 $msg='';
 
 
-$name = $_FILES['file']['name'];
-$size = $_FILES['file']['size'];
-$tmp = $_FILES['file']['tmp_name'];
-$ext = getExtension($name);
+// $name = $_FILES['file']['name'];
+// $size = $_FILES['file']['size'];
+// $tmp = $_FILES['file']['tmp_name'];
+// $ext = getExtension($name);
 
-if(strlen($name) > 0)
-{
+// if(strlen($name) > 0)
+// {
 
-if(in_array($ext,$valid_formats))
-{
+// if(in_array($ext,$valid_formats))
+// {
  
-if($size<(1024*1024))
-{
+// if($size<(1024*1024))
+// {
 include('s3_config.php');
 //Rename image name. 
+
+
+$len = count($_FILES['file']['name']);
+
+echo "number "+$len;
+
+for ($i = 0; $i < $len; $i++)
+ {
+$tmp = $_FILES['file']['tmp_name'][$i];
+$name = $_FILES['file']['name'][$i];
+$ext = getExtension($name);
 $actual_image_name = time().".".$ext;
-if($s3->putObjectFile($tmp, $bucket , $actual_image_name, S3::ACL_PUBLIC_READ) )
-{
+if($s3->putObjectFile($tmp, $bucket , $actual_image_name, S3::ACL_PUBLIC_READ) ){
 $msg = "S3 Upload Successful.";	
 $s3file='http://'.$bucket.'.s3.amazonaws.com/'.$actual_image_name;
 echo "<img src='$s3file' style='max-width:400px'/><br/>";
 echo '<b>S3 File URL:</b>'.$s3file;
 
 }
-else
-$msg = "S3 Upload Fail.";
+else{
+ $msg = "S3 Upload Fail.";
 
-
-}
-else
-$msg = "Image size Max 1 MB";
+  }
 
 }
-else
-$msg = "Invalid file, please upload image file.";
+// else{
+// $msg = "Image size Max 1 MB";
 
-}
-else
-$msg = "Please select image file.";
+// }
+// else{
+// $msg = "Invalid file, please upload image file.";
 
+// }
+// else{
+// $msg = "Please select image file.";
+// }
 
 ?>
 
