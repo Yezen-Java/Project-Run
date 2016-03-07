2fc5e7a3399ccc5d4bbc195dc67d72106b5353e2
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-03-07 19:22:57 +0000

    added file upload scrpit

--- UploadScript.php
+++ UploadScript.php
@@ -1,6 +1,5 @@
 <?php
 include('image_check.php');
-include('Connect.php');
 include('s3_config.php');
 
 $msg='';
@@ -19,13 +18,12 @@ for ($i = 0; $i < $len; $i++)
 $tmp = $_FILES['file']['tmp_name'][$i];
 $name = $_FILES['file']['name'][$i];
 $ext = getExtension($name);
-//$actual_media_name = time().".".$ext;
+$actual_media_name = time().".".$ext;
 if($s3->putObjectFile($tmp, $bucket,$name, S3::ACL_PUBLIC_READ) ){
 $msg = "S3 Upload Successful.";	
-$s3file='http://'.$bucket.'.s3.amazonaws.com/'.$name;
-$strValName = (string)$name;
-$strValLink = (string)$s3file;
-$result = pg_query("INSERT into media(media_name,link) values('$strValName','$strValLink')");
+include('Connect.php');
+$s3file='http://'.$bucket.'.s3.amazonaws.com/'.$actual_media_name;
+$result = pg_query("INSERT into media(media_name,link) values('$name','$actual_media_name')");
 echo   "<div class='col-md-3 col-sm-4 col-xs-6'>
               <img src='$s3file' alt='Mountain View' style='width:100px;height:150px;'>
             </div>";
