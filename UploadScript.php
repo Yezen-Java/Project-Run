4eb64b7f85988342265321994d3af1ac6005723d
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-03-07 19:27:44 +0000

    added file upload scrpit

--- UploadScript.php
+++ UploadScript.php
@@ -19,12 +19,12 @@ $tmp = $_FILES['file']['tmp_name'][$i];
 $name = $_FILES['file']['name'][$i];
 $ext = getExtension($name);
 $actual_media_name = time().".".$ext;
-if($s3->putObjectFile($tmp, $bucket,$name, S3::ACL_PUBLIC_READ) ){
+if($s3->putObjectFile($tmp, $bucket,$actual_media_name, S3::ACL_PUBLIC_READ) ){
 $msg = "S3 Upload Successful.";	
-include('Connect.php');
 $s3file='http://'.$bucket.'.s3.amazonaws.com/'.$actual_media_name;
-$result = pg_query("INSERT into media(media_name,link) values('$name','$actual_media_name')");
-echo   "<div class='col-md-3 col-sm-4 col-xs-6'>
+include('Connect.php');
+
+ echo "<div class='col-md-3 col-sm-4 col-xs-6'>
               <img src='$s3file' alt='Mountain View' style='width:100px;height:150px;'>
             </div>";
 
@@ -39,6 +39,9 @@ else{
   }
 
 }
+
+$result = pg_query("INSERT into media(media_name,link) values('$name','$actual_media_name')");
+
 // else{
 // $msg = "Image size Max 1 MB";
 
