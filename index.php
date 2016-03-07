5422c0c69d0867811d90ba3d61a9565fe66e6d3e
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-03-07 19:45:18 +0000

    added file upload scrpit

--- index.php
+++ index.php
@@ -230,7 +230,7 @@ $mediaResults = pg_query("SELECT * from media");
   $name = $rows['media_name'];
 
   echo "<div class='col-md-3 col-sm-4 col-xs-6'>
-              <img src='' alt='$name' style='width:100px;height:150px;'>
+              <img src='$link' alt='$name' style='width:100px;height:150px;'>
             </div>";
 }
 ?>
