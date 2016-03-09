67d8c2b74e57217db09eea1d8b99e563c9393dff
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-03-09 18:31:19 +0000

    a

--- database/DeleteMedia.php
+++ database/DeleteMedia.php
@@ -3,7 +3,7 @@
 include 's3_config.php';
 include 'Connect.php';
 
-$getArrayMedia = explode(",", $_POST['ArrayMedia']); 
+$getArrayMedia = $_POST['ArrayMedia']; 
 //$getArrayMedia = json_decode(str_replace('\\', '', $_POST['ArrayMedia']));
 //explode(",", $_POST['data']);
 
