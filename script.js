3c5f228a7e2af4b5d39300ac25cabc3052f27282
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-03-09 22:37:33 +0000

    a

--- script.js
+++ script.js
@@ -196,21 +196,27 @@ $(function(){
       };
       console.log(stringBuldier);
       console.log(Arrayofvlaues);
+
+ $.post('database/addLocationScript.php',{Array:stringBuldier}, function(data){
+        alert(data);
+       
+       });
+
      
-    $.ajax({
-url: "/database/DeleteMedia.php",
-type: "POST",
-data: {Array:stringBuldier},
-contentType: false,
-cache: false,
-processData:false,
-success: function(data){
+//     $.ajax({
+// url: "/database/DeleteMedia.php",
+// type: "POST",
+// data: {Array:stringBuldier},
+// contentType: false,
+// cache: false,
+// processData:false,
+// success: function(data){
 
-alert(data);
+// alert(data);
 
-},
-error: function(){}           
-});
+// },
+// error: function(){}           
+// });
 
     }
   });
