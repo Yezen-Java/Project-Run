90c81a8913f9898a2ab5e7467cf5f8f2414e5d80
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-03-10 21:19:23 +0000

    a

--- database/getmediaOfLocations.php
+++ database/getmediaOfLocations.php
@@ -4,18 +4,17 @@ include 'Connect.php';
 
 $getLocationId = $_POST['LocationId'];
 
-echo $getLocationId ;
 
 $getLocationMedia = pg_query("SELECT * From location_res, media where location_res.locationid = $getLocationId and location_res.mediaid = media.mediaid;");
 
 
-$rowsNumber = pg_num_rows($getLocationMedia);
+//$rowsNumber = pg_num_rows($getLocationMedia);
 
 
 
-	if($rowsNumber>0){
+	if($getLocationMedia){
 
-	    while ($rows = pg_fetch_array($getLocationMeida)) {
+	    while ($rows = pg_fetch_array($getLocationMedia)) {
 		       //$mediaId = $rows['mediaid'];
                $media_name = $rows['media_name'];
 
