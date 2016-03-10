513af628ade9d97b4a16b1fa531580dd8056f298
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-03-10 21:09:49 +0000

    a

--- database/getmediaOfLocations.php
+++ database/getmediaOfLocations.php
@@ -3,7 +3,7 @@
 $getLocationId = $_POST['LocationId'];
 
 
-$getLocationMedia = pg_query("SELECT * From location_res, media where locationid = $getLocationId");
+$getLocationMedia = pg_query("SELECT * From location_res, media where location_res.locationid = $getLocationId and location_res.mediaid = media.mediaid;");
 
 
 $rowsNumber = pg_num_rows($getLocationMedia);
