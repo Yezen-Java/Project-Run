698b0b22abbc8cd38d16543b4962a8164ede4da2
Hassan Minhas <haminhas@live.com>
2016-03-11 17:14:50 +0000

    hassan > sedar

--- script.js
+++ script.js
@@ -113,7 +113,7 @@ function deleteTourLi(tourId){
     for (var x = 0; x < 6; x++) {
         var item = $('#buttonsListTours').children('li').get(x);
         console.log(item);
-        console.log(item.data('value'));
+        console.log(item.data('val'));
          if(item.value == tourId){
           item.remove();
          }
