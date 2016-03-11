19c6a63056de4ea447e400403ada52b61105fbe5
Hassan Minhas <haminhas@live.com>
2016-03-11 16:21:39 +0000

    hassan > sedar

--- index.php
+++ index.php
@@ -130,7 +130,7 @@ $MediaSelect = pg_query("SELECT * from media");
             $tour_id =$rows["tourid"];
             $tour_name =$rows["tour_name"];
 
-          echo "<li class='tourButtonsList' value='$tour_id'> 
+          echo "<li value='$tour_id'> 
               <div class='input-group'> 
               <span class='input-group-addon'> 
                 <button class='glyphicon glyphicon-trash' id='$tour_id' onclick='deleteTourLi(this.id, this)'></button> 
