83a1c9b584ce8c6df97481680685880090dffc4d
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-03-11 18:37:56 +0000

    added delete tour script

--- script.js
+++ script.js
@@ -116,7 +116,14 @@ function deleteTourLi(tourId){
         console.log(item);
         console.log(item.id);
          if(item.id == tourId){
-          item.remove();
+          $(function() {
+       $.post('database/DeleteTours.php',{TourID:tourId}, function(data){
+        alert(data);
+        item.remove();
+       });
+       return false;
+  });
+
          }
     }
       // $(".tourButtons").get(i).remove();
