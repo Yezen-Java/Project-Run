70eb5dd8eee265098ff3485cc96b1a9d965dfa8c
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-03-13 15:20:39 +0000

    added prepare statement for login session

--- database/AddMediaToLocation.php
+++ database/AddMediaToLocation.php
@@ -3,44 +3,45 @@
 include 'Connect.php';
 
 
+$location = $_POST['locationNo'];
 $lis = $_POST['items'];
 $liarray = explode("::", $lis);
 
 
 $le = count($liarray);
-echo $le;
+echo "number $le and location number $location ";
 
-for ($i=0; $i <$le; $i++) { 
 
-echo $liarray[$i];	
+
+function FunctionName(){
+
+for ($i=0; $i < $le ; $i++) { 
+	# code...
+	$query = "INSERT into location_res(locationid,link) values ($1,$2)";
+
+	$result = pg_prepare($dbconn,"query", $query);
+
+	$result = pg_execute($dbconn,"query",  array($location,$liarray[$i]));
+
+	$cmdtuples = pg_affected_rows($result);
+
+	if ($cmdtuples > 0) {
+			
+		echo"MeidaId $meidaId been added to LocationId $location";
+
+	}else{
+
+		echo "Faild";
+	}
+
+	sleep(1);
+}
 
 }
 
+FunctionName();
 
 
 
- function FunctionName(){
-
-$meidaId = $_POST['MeidaID'];
-$location = $_POST['LocationID'];
-
-$query = "INSERT into location_res(locationid,link) values ($1,$2)";
-
-$result = pg_prepare($dbconn,"query", $query);
-
-$result = pg_execute($dbconn,"query",  array($nameData,$nameLink));
-
-
-if ($result) {
-
-	echo"MeidaId "+$meidaId+"been added to LocationId"+$location;
-}else{
-
-echo "Faild";
-
-}
-
-sleep(1);
-}
 
 ?>
