138bb44bb06303f4edf3bd3df74625b6eb4b14d9
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-03-14 23:32:56 +0000

    ad

--- database/addLocationScript.php
+++ database/addLocationScript.php
@@ -32,25 +32,25 @@ echo $tourid;
 //    	$this->databaseConnection = $dbconn;
 //    }
 
-echo getLocation();
+// echo getLocation();
 
-	function getLocation(){
-		global $dbconn;
-		global $tourId;
+// 	function getLocation(){
+// 		global $dbconn;
+// 		global $tourId;
 
-		$query= "SELECT * from tour_res where tourid = $1"; 
-		$results =pg_prepare($dbconn, "selectquery",$tourId);
+// 		$query= "SELECT * from tour_res where tourid = $1"; 
+// 		$results =pg_prepare($dbconn, "selectquery",$tourId);
 
-		$results =pg_execute($dbconn,"selectquery",array("TOR124"));
+// 		$results =pg_execute($dbconn,"selectquery",array("TOR124"));
 
-	    $menu = array();
-        while ($row = mysql_fetch_array($query)) {
-	        $menu[] = array($row['id']);
+// 	    $menu = array();
+//         while ($row = mysql_fetch_array($query)) {
+// 	        $menu[] = array($row['id']);
 	  
-        }
-        return json_encode($menu);
+//         }
+//         return json_encode($menu);
 
-	} 
+// 	} 
 
 	// function deleteFromUnchecked(){
 
