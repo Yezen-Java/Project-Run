fbdc18a2fd796a4636a27ea619dbfff575332308
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-03-14 23:36:33 +0000

    ad

--- database/AddLocationClass.php
+++ database/AddLocationClass.php
@@ -1,9 +1,33 @@
 <?php 
 
+Include'Connect.php';
 
 $tourId = $_POST['TourID'];
 
 echo $tourid;
+
+
+echo getLocation();
+
+
+
+	function getLocation(){
+		global $dbconn;
+		global $tourId;
+
+		$query= "SELECT * from tour_res where tourid = $1"; 
+		$results =pg_prepare($dbconn, "selectquery",$tourId);
+
+		$results =pg_execute($dbconn,"selectquery",array("TOR124"));
+
+	    $menu = array();
+        while ($row = mysql_fetch_array($query)) {
+	        $menu[] = array($row['id']);
+	  
+        }
+        return json_encode($menu);
+
+	} 
 
 
 ?>
