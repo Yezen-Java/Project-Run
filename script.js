ce5dd598ca2acc32093b8b0465f8514548fb881c
Hassan Minhas <haminhas@live.com>
2016-03-15 18:41:57 +0000

    location manager

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ $(document).ready(function(){
       // loop over them all
       for (var i=0; i<checkboxes.length; i++) {
          // And stick the checked ones onto an array...
-         console.log($(checkboxes[i]).value);
+         console.log($(checkboxes[i]).id);
          if(selectedID.indexOf($(checkboxes[i]).value) > 0){
             $(checkboxes[i]).prop('checked', true);
          }
