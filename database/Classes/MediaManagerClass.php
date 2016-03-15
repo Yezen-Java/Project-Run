ebdbef2a3989497cdb2a9280a176136514700b7d
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-03-15 13:02:38 +0000

    addes classes

--- database/Classes/MediaManagerClass.php
+++ database/Classes/MediaManagerClass.php
@@ -10,24 +10,6 @@ class MediaManager
 {
 
 
-private $s3;
-use Aws\S3\S3Client;
-
-public function amazonS3(){
-
-$bucket="storage.s3.website.com";
-if (!class_exists('S3'))require('s3.php');
-
-			
-if (!defined('awsAccessKey')) define('awsAccessKey', 'AKIAJQAUHJ7XGYHTS6AQ');
-if (!defined('awsSecretKey')) define('awsSecretKey', '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');
-			
-$s3 = new S3(awsAccessKey, awsSecretKey);
-
-$s3->putBucket($bucket, S3::ACL_PUBLIC_READ);
-
-}
-
 public function addLocationTour($le,$liarray,$location,$dbconn){
 
     $query = "INSERT into location_res(locationid,mediaid) values ($1,$2);";
@@ -54,6 +36,7 @@ public function addLocationTour($le,$liarray,$location,$dbconn){
 public function deleteMeida($arrayMedia){
 
 	global $dbconn;
+	global $s3;
 	$checkDelete = 0;
 
 $query = "DELETE From media where mediaid = $1";
