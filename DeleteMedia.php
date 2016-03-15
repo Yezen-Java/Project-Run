d07ce37147f82e048d129ee9aa7f322e43815268
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-03-15 12:33:48 +0000

    addes classes

--- DeleteMedia.php
+++ DeleteMedia.php
@@ -2,7 +2,7 @@
 
 include 's3_config.php';
 include 'database/Connect.php';
-include 'Classes/MediaManagerCLass.php';
+include 'Classes/MediaManagerClass.php';
 
 $mediaid = $_POST['checkboxmedia'];
 
