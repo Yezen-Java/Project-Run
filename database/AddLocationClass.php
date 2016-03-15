d85c07ef54decb5024132446b27c81ba8cba334e
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-03-15 00:10:00 +0000

    ad

--- database/AddLocationClass.php
+++ database/AddLocationClass.php
@@ -11,7 +11,7 @@ $le = count($liarray);
 
 	function insertLocation(){
 		global $tourId;
-        global $locationIdArray;
+        global $liarray;
 
 	$addLocationTourQ = "INSERT into tour_res values ($1,$2)";
 	$addLocationQueryt = pg_prepare($dbconn,"addLocationQuery", $addLocationTourQ);
