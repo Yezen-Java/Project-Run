cfdcd50dfdc0af94838eef7a8e6cbf8dc04e01c7
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-03-15 08:38:39 +0000

    insert tour

--- database/Insertour.php
+++ database/Insertour.php
@@ -2,7 +2,8 @@
 
 error_reporting(E_ALL & ~E_NOTICE);
 session_start();
-include "Connect.php";
+require "Connect.php";
+include 'TourClass.php';
 
 $tourID = $_POST['TourID'];
 $tourName = $_POST['Tourname'];
@@ -14,26 +15,36 @@ $escapeTName = pg_escape_string($tourName);
 $escapeTD = pg_escape_string($tourDate);
 $escapeUser = pg_escape_string($username);
 
-$TourIDCheck = pg_query("SELECT * from tour where tourid = '$escapeTourid'");
 
-$TourNumberIDs = pg_num_rows($TourIDCheck);
-
-if ( $TourNumberIDs == 0) {
-	$tourQuery = pg_query("INSERT INTO tour Values('$escapeTourid', '$escapeTName','$escapeTD')");
-    $result = pg_query("INSERT INTO usertour Values('$escapeUser','$escapeTourid')");
-
-if ($tourQuery && $result) {
-	echo "TourCreated";
+$tourClass = new TourCLass();
 
 
-	}else{
-		echo $username;
-	}
+
+echo $tourClas->insertTour($escapeTourid,$escapeTName,$escapeTD,$escapeUser);
+
+
+
+
+// $TourIDCheck = pg_query("SELECT * from tour where tourid = '$escapeTourid'");
+
+// $TourNumberIDs = pg_num_rows($TourIDCheck);
+
+// if ( $TourNumberIDs == 0) {
+// 	$tourQuery = pg_query("INSERT INTO tour Values('$escapeTourid', '$escapeTName','$escapeTD')");
+//     $result = pg_query("INSERT INTO usertour Values('$escapeUser','$escapeTourid')");
+
+// if ($tourQuery && $result) {
+// 	echo "TourCreated";
+
+
+// 	}else{
+// 		echo $username;
+// 	}
 	
-}else{
+// }else{
 
-	echo "Tour ID already exsits";
-}
+// 	echo "Tour ID already exsits";
+// }
 
 
 
---
+++ database/TourClass.php
@@ -0,0 +1,54 @@
+<?php
+
+include 'Connect.php';
+
+
+/**
+* 
+*/
+
+class TourClass
+{
+	public function insertTour($escapeTourid,$escapeTName,$escapeTD,$escapeUser){
+
+		$msg ='';
+
+		$TourIDCheck = pg_query("SELECT * from tour where tourid = '$escapeTourid'");
+
+		$TourNumberIDs = pg_num_rows($TourIDCheck);
+
+		if ( $TourNumberIDs == 0) {
+			$tourQuery = pg_query("INSERT INTO tour Values('$escapeTourid', '$escapeTName','$escapeTD')");
+			$result = pg_query("INSERT INTO usertour Values('$escapeUser','$escapeTourid')");
+
+			if ($tourQuery && $result) {
+				$msg = "TourCreated";
+
+
+				}else{
+					$msg ='Some went wrong';
+				}
+				
+			}else{
+
+				$msg ="Tour ID already exsits";
+			}
+
+
+			return $msg;
+
+
+
+	}
+
+
+
+	
+
+}
+
+
+
+
+
+?>
