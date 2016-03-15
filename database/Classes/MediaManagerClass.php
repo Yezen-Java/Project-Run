e1239f863ec734931d0400fb649bf19293aacd4a
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-03-15 13:00:07 +0000

    addes classes

--- database/Classes/MediaManagerClass.php
+++ database/Classes/MediaManagerClass.php
@@ -3,12 +3,30 @@
 
 include 's3_config.php';
 include 'Connect.php';
-include'image_check.php';
+include 'image_check.php';
 
 
 class MediaManager 
 {
 
+
+private $s3;
+use Aws\S3\S3Client;
+
+public function amazonS3(){
+
+$bucket="storage.s3.website.com";
+if (!class_exists('S3'))require('s3.php');
+
+			
+if (!defined('awsAccessKey')) define('awsAccessKey', 'AKIAJQAUHJ7XGYHTS6AQ');
+if (!defined('awsSecretKey')) define('awsSecretKey', '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');
+			
+$s3 = new S3(awsAccessKey, awsSecretKey);
+
+$s3->putBucket($bucket, S3::ACL_PUBLIC_READ);
+
+}
 
 public function addLocationTour($le,$liarray,$location,$dbconn){
 
@@ -36,7 +54,6 @@ public function addLocationTour($le,$liarray,$location,$dbconn){
 public function deleteMeida($arrayMedia){
 
 	global $dbconn;
-	global $s3;
 	$checkDelete = 0;
 
 $query = "DELETE From media where mediaid = $1";
@@ -52,7 +69,7 @@ foreach ($arrayMedia as $number) {
 	$rows = pg_fetch_array($amazonQuery);
 	$mediaExt = $rows['ext_name'];
 	
-	if ($s3->deleteObject($bucket,$mediaExt)) {
+	if ($this->s3->deleteObject($bucket,$mediaExt)) {
 	 $result = pg_execute($dbconn,"query",  array($number));
 
 	 $checkDelete++;
