2ad5a2f6d85c0761c199647155e66a261861ea6b
Hassan Minhas <haminhas@live.com>
2016-03-16 16:47:25 +0000

    image manger

--- DeleteMedia.php
+++ DeleteMedia.php
@@ -18,24 +18,18 @@ $result = pg_prepare($dbconn,"query", $query);
 $amazonQuery = pg_prepare($dbconn,"query2", $query2);
 
 
-foreach ($liarray as $number) {
-
-	$amazonQuery = pg_execute($dbconn,"query2",  array($number));
+for ($i=0; $i < $numberOfMedia; $i++) { 
+	$amazonQuery = pg_execute($dbconn,"query2",  array($liarray[$i]));
 	$rows = pg_fetch_array($amazonQuery);
 	$mediaExt = $rows['ext_name'];
 	
 	if ($s3->deleteObject($bucket,$mediaExt)) {
-	 $result = pg_execute($dbconn,"query",  array($number));
-
-        	echo 'Deleted';
-
+		$result = pg_execute($dbconn,"query",  array($liarray[$i]));
+		echo 'Deleted';
 	}else{
 		echo "error";
-}
-
-
-sleep(1);
-
+	}
+	sleep(0.5);
 }
 
 
