91c72240f8a99b74348fe6b262194159e9544028
Sedar Olmez <SedarOlmez@Sedars-MacBook-Air.local>
2016-03-16 21:03:57 +0000

    test

--- index.php
+++ index.php
@@ -191,21 +191,12 @@ $LoadOnStart = new LoadOnStart();
                 </div>
             <div class="modal-footer">
 
-              <!--<progress id="uploadFileProgress" value="0" max="100"></progress>-->
-              <!-- <div class="progress">
-                  <div class ="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="70" aria-valuemin="0"
-                  aria-valuemax="100" style="width:70%">
-                    70% Complete
-                  </div>
-              </div> -->
-
+              <form id = 'uploadForm' action="" method="post" enctype="multipart/form-data">
               <div id="progressbar-5">
                 <div class="progress-label">
                   <h4>Loading....</h4>
                 </div>
               </div>
-
-              <form id = 'uploadForm' action="" method="post" enctype="multipart/form-data">
                 <div style='margin:10px'><input type='file'  name='file[]' multiple=""/> 
                   <button type="button" class="btn btn-warning" id="deleteCheckedItems">Delete</button>
                   <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
