f2aea91bd338fa57846fb52619078ce983e26795
Sedar Olmez <SedarOlmez@Sedars-MacBook-Air.local>
2016-03-16 21:08:19 +0000

    test

--- script.js
+++ script.js
@@ -119,6 +119,28 @@ $(document).ready(function(){
 /*
   progress bar jQuery for transition
 */
+    $(function(){
+        var progressbar = $("#progressbar-5");
+        progressLabel = $(".progress-label");
+        $("#progressbar-5").progressbar({
+          value : false,
+          change:function(){
+            progressLabel.text(
+              progressbar.progressbar("value") + "%");
+          },
+          complete:function(){
+            progressLabel.text("Loading Completed!");
+          }
+        });
+        function progress(){
+          var val = progressbar.progressbar("value") || 0;
+          progressbar.progressbar("value", val + 1);
+          if(val < 99){
+            setTimeout(progress, 100);
+          }
+        }
+        setTimeout(progress,3000);
+    });
 });
 
 // $("#file-manager").click(function(){
@@ -440,29 +462,6 @@ function getCheckedBoxes(element) {
         }
       });
     }
-
-      $(function(){
-      var progressbar = $("#progressbar-5");
-      progressLabel = $(".progress-label");
-      $("#progressbar-5").progressbar({
-        value : false,
-        change:function(){
-          progressLabel.text(
-            progressbar.progressbar("value") + "%");
-        },
-        complete:function(){
-          progressLabel.text("Loading Completed!");
-        }
-      });
-      function progress(){
-        var val = progressbar.progressbar("value") || 0;
-        progressbar.progressbar("value", val + 1);
-        if(val < 99){
-          setTimeout(progress, 100);
-        }
-      }
-      setTimeout(progress,3000);
-    });
 
 
 
