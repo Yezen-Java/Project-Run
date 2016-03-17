5554a2300b899424f7cb0ae076fa135875632d97
Hassan Minhas <haminhas@live.com>
2016-03-17 22:42:00 +0000

    notes search

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
       console.log($("#search").val());
       
       $(".searching li").each(function(index,object){
-        console.log($(object));
+        console.log($(object).innerHTML);
       
       });
 
