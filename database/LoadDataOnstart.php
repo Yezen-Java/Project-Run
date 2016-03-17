3509a9401e2057eb33717587f1529563f953e09a
Hassan Minhas <haminhas@live.com>
2016-03-17 11:42:45 +0000

    edit tour

--- database/LoadDataOnstart.php
+++ database/LoadDataOnstart.php
@@ -21,9 +21,11 @@ if (pg_num_rows($toursListQuery)>0) {
             $tour_id =$rows["tourid"];
             $tour_name =$rows["tour_name"];
         echo "<li id='$tour_id'> 
-                <div class='input-group'>
-                  <div class='btn-group' role='group' aria-label='...''>  
-                    <button class='glyphicon glyphicon-trash' id='$tour_id' onclick='deleteTourLi(this.id)'></button>   
+                <div class='btn-group' role='group' aria-label='...''> 
+                  <div class='input-group'>
+                    <span class='input-group-addon'> 
+                      <button class='glyphicon glyphicon-trash' id='$tour_id' onclick='deleteTourLi(this.id)'></button> 
+                    </span> 
                     <button type='button' class='list-group-item tourButtons' value='$tour_id' onclick='w3_open(this.value)'>$tour_name $tour_id</button>
                     <button type='button' class='btn btn-success'>Edit</button>
                   </div>
