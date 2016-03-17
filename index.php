a693dfa92372be684dc45b09c53c3cb7c6564f9b
Hassan Minhas <haminhas@live.com>
2016-03-17 13:12:49 +0000

    edit tour

--- index.php
+++ index.php
@@ -40,7 +40,7 @@ $LoadOnStart = new LoadOnStart();
 
     <!--CSS LINK-->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/lib/w3.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/datepicker.css">
     <!--JS LINK-->
