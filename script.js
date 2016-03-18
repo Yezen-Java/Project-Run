bea96a0320da082d0fd3cf6c8c2b40bfd9acc5ba
Hassan Minhas <haminhas@live.com>
2016-03-18 15:31:05 +0000

    search

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
       $(".searching").empty();
 
       $(listItems).each(function(index,object){
-        if (!$("#search").val()){
+        if ($('#search').val() !== ''){
           console.log("works");
           $(".searching").append($(object));
         } else {
