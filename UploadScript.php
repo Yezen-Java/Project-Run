174b97e26bbbc5f41b38df75f6ff48d7f00f9cdd
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-03-18 09:46:29 +0000

    sperated the s3 script and mreged

--- UploadScript.php
+++ UploadScript.php
@@ -1,68 +1,23 @@
 <?php
 
 
-// include'database/Classes/MediaManagerClass.php';
-// include'database/Connect.php';
-// include'database/LoadDataOnstart.php';
-// include'database/Classes/s3/image_check.php';
-// include'database/Classes/s3/s3_config.php';
-
-
-
-// $uploadmedia = new MediaManager();
-// $checkUpload = $uploadmedia->mediaUpload($_FILES['file'],$dbconn,$s3);
-// $loadDataUploaded = new LoadOnStart();
-// $htmlTageOuput = $loadDataUploaded->admediaResultsFucntion();
-
-// if($checkUpload){
-
-//   echo $htmlTageOuput;
-
-// }else{
-
-//   echo $checkUpload;
-// }
-
-
-
 include('image_check.php');
 include('database/Connect.php');
 include('s3_config.php');
 include'database/LoadDataOnstart.php';
 
-$_files = $_FILES['file'];
 
-$checkUpload = uploadMedia();
-
-
-if($checkUpload){
-
-$loadDataUploaded = new LoadOnStart();
-$htmlTageOuput = $loadDataUploaded->admediaResultsFucntion();
-echo $htmlTageOuput;
-}else{
-  echo false;
-}
-
-
-
-function uploadMedia(){
-
-global $dbconn;
-global $s3;
-global $_files;
-
-$len = count($_files['file']['name']);
-$msg='';
+$len = count($_FILES['file']['name']);
+$msg=false;
 $query = "INSERT into media (media_name,link,ext_name,media_type) values ($1,$2,$3,$4)";
 $result = pg_prepare($dbconn,"query", $query);
 $sizeLimit = 2097152;
 
 for ($i = 0; $i < $len; $i++){
 
-  $tmp = $_files['file']['tmp_name'][$i];
-  $name = $_files['file']['name'][$i];
-  $size = $_files['file']['size'][$i];
+  $tmp = $_FILES['file']['tmp_name'][$i];
+  $name = $_FILES['file']['name'][$i];
+  $size = $_FILES['file']['size'][$i];
   $ext = getExtension($name);
   $actual_media_name = time().".".$ext;
   $MediaType = '';
@@ -83,31 +38,37 @@ for ($i = 0; $i < $len; $i++){
               $s3file='http://'.$bucket.'.s3.amazonaws.com/'.$actual_media_name;
               $nameData = $name;
               $nameLink = $s3file;
-                pg_execute($dbconn,"query", array($name,$s3file,$actual_media_name,$MediaType));
+               if(pg_execute($dbconn,"query", array($name,$s3file,$actual_media_name,$MediaType))){
+
+                $msg=false;
+
+               }else{
+                $msg=false;
+               }
 
         }
   else{
 
-    return false;
+$msg=false;
 
   }
 
 }else{
 
 
-    return false;
+$msg=false;
 }
 
 }else{
 
 
-    return false;
+$msg=false;
 
 }
 
 }else{
 
-    return false;
+$msg=false;
 }
 
  sleep(2);
@@ -115,9 +76,13 @@ for ($i = 0; $i < $len; $i++){
 }
 
 
-return true;
-
+if ($msg) {
+  $loadData = new LoadDataOnstart();
+   echo $loadData->mediaResultsFucntion();
+}else{
+  echo false;
 }
+
 
 
 ?>
