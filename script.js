2e371606882e8eddb529003345eb975e2536f8cf
Hassan Minhas <haminhas@live.com>
2016-03-18 20:47:04 +0000

    location manager

--- script.js
+++ script.js
@@ -512,7 +512,7 @@ function getCheckedBoxes(element) {
               arrayofvalues.push($(this).val());
           });
           console.log(arrayofvalues);
-          if (arrayofvalues != null){
+          if (arrayofvalues !== []){
             console.log("It didn't work!");
             $.post('database/DeleteLocation.php',{LocationIds:arrayofvalues.join("::")}, function(data){
                 if(data == false){
