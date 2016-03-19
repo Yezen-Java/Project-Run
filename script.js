67bea6c257ac57270f5f4a96238bd869a6599379
Hassan Minhas <haminhas@live.com>
2016-03-19 19:53:45 +0000

    sortable

--- script.js
+++ script.js
@@ -394,8 +394,9 @@ console.log("test "+locationID);
 
        $.post('database/getmediaOfLocations.php',{LocationId:locationID}, function(data){
         $("#listMedia").append(data);
+        $('#listMedia').sortable();
+
        });
-               $('.sortable').sortable();
 
        return false;
   });
