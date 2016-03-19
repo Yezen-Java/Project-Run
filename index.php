890126f8233c3245aad79bbbe681ad0993cc3585
Sedar Olmez <SedarOlmez@Sedars-MacBook-Air.local>
2016-03-19 22:07:03 +0000

    accountManager

--- index.php
+++ index.php
@@ -29,7 +29,6 @@ $LoadOnStart = new LoadOnStart();
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
 
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
