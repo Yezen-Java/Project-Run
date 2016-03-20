84077402d05a457a1d671414fd756bb77ea4e98b
Sedar Olmez <SedarOlmez@Sedars-MacBook-Air.local>
2016-03-20 14:40:35 +0000

    testing

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ $LoadOnStart = new LoadOnStart();
 <nav class="navbar navbar-default navbar-fixed-top">
   <div class="container">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>                        
