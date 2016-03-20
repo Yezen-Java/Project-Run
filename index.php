55349ad32f298c418a3d12a545677edfe49ed5ec
Sedar Olmez <SedarOlmez@Sedars-MacBook-Air.local>
2016-03-20 15:03:20 +0000

    testing

--- index.php
+++ index.php
@@ -56,34 +56,6 @@ $LoadOnStart = new LoadOnStart();
 
 <body onload="myFunction()">
 
-<!-- <nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-      <a class="navbar-brand" href="#"><strong>HIVE</strong>Dashboard</a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li><a href="#" id="menu-toggle">Note</a></li>
-        <li class="dropdown">
-          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Manager  <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#file_manager_dialogue" id="file-manager" data-toggle="modal">File Manager</a></li>
-            <li><a href="#LocationMangerModal" id="location-manager" data-toggle="modal">Location Manager</a></li>
-            <li><a href="#account-manager-modal" id="account-manager" data-toggle="modal">Account Manager</a></li>
-          </ul>
-        </li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li id="logoutButton"><a href="#logoutModal" data-toggle="modal"><span class="glyphicon glyphicon-log-out"></span>Logout</a></li>
-      </ul>
-    </div>
-  </div>
-</nav> -->
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
@@ -141,13 +113,11 @@ $LoadOnStart = new LoadOnStart();
             echo $LoadOnStart->getTourList();
           ?>
           </ul>
-          </div>
-        
-        
+          </div>  
         <div class="btn-group" role="group" aria-label="Basic example" id="tourButtonGroup">
           <button id="addTour" type="button" class="btn btn-success"><strong>Add</strong></button>
         </div>
-            </div>
+      </div>
 
     <!--Below we have the notes nav bar wrapped, here we should be able to add new notes, delete notes and
     so forth.-->
