e8b2f0c4f8d2fbfd32fe6d871bb79c90f7b652f6
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-03-20 10:55:50 +0000

    added user locaiton

--- database/AddMediaToLocation.php
+++ database/AddMediaToLocation.php
@@ -1,8 +1,13 @@
 <?php
 
+
+error_reporting(E_ALL & ~E_NOTICE);
+session_start();
+
 include 'Connect.php';
 include 'Classes/MediaManagerClass.php';
 
+$username = $_SESSION['username'];
 
 $location = $_POST['locationNo'];
 $lis = $_POST['items'];
@@ -10,7 +15,7 @@ $liarray = explode("::", $lis);
 $le = count($liarray);
 
 $Addmedia = new MediaManager();
-$getResults = $Addmedia-> addMeidaToLocation($le,$liarray,$location,$dbconn);
+$getResults = $Addmedia-> addMeidaToLocation($le,$liarray,$location,$dbconn,$username);
 
 echo $getResults;
 
