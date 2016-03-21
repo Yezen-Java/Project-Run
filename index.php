33398646812ba5e949cbac1ac680efc20abcf946
Sedar Olmez <SedarOlmez@Sedars-MacBook-Air.local>
2016-03-21 17:34:17 +0000

    fix

--- index.php
+++ index.php
@@ -129,9 +129,13 @@ $LoadOnStart = new LoadOnStart();
                 <li class="sidebar-brand" style="padding-top:10px;">
                   <div id="container">
                     <form name="form1" method="post" action="">
-                      <input type="text" class="form-control" id="search" placeholder ="Search">
-                      <input type="text" class="form-control" name="toDoTextArea" id="toDoTextArea" autofocus placeholder="Message">
-                      <button type="button" class="btn btn-default-success" name="addBtn" id="addBtn" autofocus>Add</button>
+                      <input type="text" class="form-control" id="search" placeholder ="Search"> 
+                      <div class="input-group">
+                        <input type="text" class="form-control" name="toDoTextArea" id="toDoTextArea" autofocus placeholder="Message">
+                        <span class="input-group-btn">
+                        <button type="button" class="btn btn-default-success" name="addBtn" id="addBtn" autofocus>Add</button>
+                        </span>
+                      </div>
                     </form>
                     <ul id="myList" class="sortable list searching"></ul>
                     <button class="btn btn-danger" id="btnClear"><a id="clearAll" href="javascript:void();">Delete All</a></button>
