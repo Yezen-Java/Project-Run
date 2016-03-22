5f436f96cd5d5f8aaed48cdcdfba56c8ef84b35c
Sedar Olmez <SedarOlmez@Sedars-MacBook-Air.local>
2016-03-22 22:55:54 +0000

    userAccount retrieve ID

--- index.php
+++ index.php
@@ -127,17 +127,15 @@ $LoadOnStart = new LoadOnStart();
           <ul class="sidebar-nav" id="sideBar">  
                 <li class="sidebar-brand" style="padding-top:10px;">
                     <form name="form1" id="notesBarHeight" method="post" action="">
-                    <div class="container" id="notesBarItemsTop">
                         <input type="text" class="form-control" id="search" placeholder ="Search"> 
-                          <div class="input-group">
+                          <div class="input-group" id="notesBarItemsTop">
                             <input type="text" class="form-control" placeholder="Message" id="toDoTextArea">
                             <span class="input-group-btn">
                               <button class="btn btn-default" type="button" id="addBtn">Add</button>
                             </span>
                           </div>
-                    </div>
                       <div class="container" id="notesBarContainer">
-                        <div class="container" id="buttonContainer">
+                      <div class="container" id="buttonContainer">
                           <button type="button" class="btn btn-danger" id="btnClear"><a href="javascript:void();" id="clearAll">Delete All</a></button>
                         </div> 
                           <ul id="myList" class="sortable list searching"></ul>
