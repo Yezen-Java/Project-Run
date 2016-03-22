56ff654f8d055ea111fd09e4284d698a3bd9ade8
Sedar Olmez <SedarOlmez@Sedars-MacBook-Air.local>
2016-03-22 23:24:33 +0000

    userAccount retrieve ID

--- index.php
+++ index.php
@@ -140,14 +140,15 @@ $LoadOnStart = new LoadOnStart();
                     <div class="container" id="buttonContainer">
                       <button class="btn btn-danger" id="clearAll"><a href="javascript:void();" id="clearAll">Delete All</a></button>
                     </div>
-                    <div class="container" id="leftNotesCont">
-                      <ul id="myList" class="sortable list searching"></ul>
-                    </div>
+                    
                   </div>
                 </li>
           </ul>
         </div>
     </div>
+    <div class="container" id="leftNotesCont">
+                      <ul id="myList" class="sortable list searching"></ul>
+                    </div>
 </div>
   <!--Create tour dialogue box-->
   <div class="modal fade" id="createTourDialogue" role="dialog">
