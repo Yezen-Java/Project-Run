94692ac0e0227d19c949b639ab782b535a240307
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-04-06 17:00:44 +0100

    added new amazon storage details

--- s3/s3_config.php
+++ s3/s3_config.php
@@ -1,12 +1,12 @@
 <?php
 
 use Aws\S3\S3Client;
-$bucket="hive.testing.storage";
+$bucket="hive.storage";
 if (!class_exists('S3'))require('s3.php');
 
 			
-if (!defined('awsAccessKey')) define('awsAccessKey', 'AKIAJQAUHJ7XGYHTS6AQ');
-if (!defined('awsSecretKey')) define('awsSecretKey', '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');
+if (!defined('awsAccessKey')) define('awsAccessKey', 'AKIAJBRJTSIXZGEMTSXQ');
+if (!defined('awsSecretKey')) define('awsSecretKey', '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');
 			
 $s3 = new S3(awsAccessKey, awsSecretKey);
 
